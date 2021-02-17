2f4480e514754f21a90d176360f4603fecbe7059
João Pedro <Joo Pedro@DESKTOP-92J5HP3>
2021-02-17 14:45:07 -0300

    remove @RequestParam from addNewUser (controller)

--- ListaDragoes/src/main/java/com/example/listadragoes/api/controllers/DragaoController.java
+++ ListaDragoes/src/main/java/com/example/listadragoes/api/controllers/DragaoController.java
@@ -30,8 +30,8 @@ public class DragaoController {
 private DragaoRepository dragaoRepository;
 
 @PostMapping(path="/add") // Map ONLY POST Requests
-public @ResponseBody String addNewUser (@RequestParam String name
- , @RequestParam String email) {
+public @ResponseBody String addNewUser (String name
+ ,String email) {
 // @ResponseBody means the returned String is the response, not a view name
 // @RequestParam means it is a parameter from the GET or POST request
 
