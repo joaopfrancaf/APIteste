42ecba0e9d989177a91c53ab1ded0c81acafb5aa
João Pedro <Joo Pedro@DESKTOP-92J5HP3>
2021-02-17 14:49:20 -0300

    pom file changes

--- ListaDragoes/src/main/java/com/example/listadragoes/api/controllers/DragaoController.java
+++ ListaDragoes/src/main/java/com/example/listadragoes/api/controllers/DragaoController.java
@@ -3,8 +3,6 @@ import com.example.listadragoes.api.entities.Dragao;
 import java.util.List;
 import java.util.Optional;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
